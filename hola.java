9f525307d82ebe7618c6c6cfcf980534e374c7a3
EduardMR <eduard.martinez.rama@gmail.com>
2016-03-02 18:20:35 +0100

    segunda partesegunda parte

--- hola.java
+++ hola.java
@@ -2,7 +2,9 @@ public class hola{
 	public static void main(String[] arg){
 	
 
-		System.out.println("Si un millon de moscas caomen mierda no pueden estar equivocadas, come mierda");
+		System.out.println("Si un millon de moscas caomen mierda no pueden estar equivocadas, come mierda" + arg[0]);
+
+
 
 	}
 }
