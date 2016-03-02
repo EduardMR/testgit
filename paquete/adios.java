d3a46d00f8113013ced845e4fdb0fb6fac367c0d
EduardMR <eduard.martinez.rama@gmail.com>
2016-03-02 20:59:16 +0100

    cambio directiorio

--- paquete/adios.java
+++ paquete/adios.java
@@ -1,9 +1,10 @@
 public class hola{
 
 	static String nombre = "Jose";
-
+	
 	public static void main(String[] arg){
-
+		
+		//a liarla
 		System.out.println("Si un millon de moscas caomen mierda no pueden estar equivocadas, come mierda " + nombre);
 
 	}
